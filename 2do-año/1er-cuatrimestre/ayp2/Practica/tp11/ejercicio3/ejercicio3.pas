35cf0efb2c7f0f31b9b29bbadf859a70dc9dd702
Giuliano Poeta <poetaignacio@gmail.com>
2023-05-31 16:10:13 -0300

    se termino el tp 10 y se empezo el 11

---
+++ 2do-año/1er-cuatrimestre/ayp2/Practica/tp11/ejercicio3/ejercicio3.pas
@@ -0,0 +1,4 @@
+{
+    Escribir un procedimiento para suprimir un nodo de una lista doblemente enlazada, lineal, 
+    cuyo apuntador apunta a cualquier nodo de la lista, no necesariamente a un extremo. 
+}
